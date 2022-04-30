a51c533d062e8e7de9410bf6ad4279914613a68c
mohamedAllam <mf7059981@gmail.com>
2022-04-30 22:04:51 +0200

    creat new file

---
+++ assets/js/Them.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+  
+
+    var all_panels = $('.templatemo-accordion > li > ul ').hide();
+
+    $('.templatemo-accordion > li .slide1').click(function(){
+       $(".shop-one").slideDown();
+    });
+  
+ 
+
+
+    
+});
+
+
+
+
+
